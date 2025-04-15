0ea1065cb842f74879aec2bb710c23fff8b69d67
thrasosc <100732009+thrasosc@users.noreply.github.com>
2025-04-15 11:09:56 +0200

    create block renderer

--- src/main/java/net/pixeldreamstudios/valor_blocks/ValorBlocksClient.java
+++ src/main/java/net/pixeldreamstudios/valor_blocks/ValorBlocksClient.java
@@ -1,9 +1,14 @@
 package net.pixeldreamstudios.valor_blocks;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.minecraft.client.renderer.RenderType;
+import net.pixeldreamstudios.valor_blocks.registry.BlockRegistry;
 
 public class ValorBlocksClient implements ClientModInitializer {
 
   @Override
-  public void onInitializeClient() {}
+  public void onInitializeClient() {
+    BlockRenderLayerMap.INSTANCE.putBlock(BlockRegistry.CAGE, RenderType.cutoutMipped());
+  }
 }
