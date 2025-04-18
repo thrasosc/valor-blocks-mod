ab0177b29126b98b623fc0e60551fd640755f399
thrasosc <100732009+thrasosc@users.noreply.github.com>
2025-04-19 00:34:41 +0200

    change Sittingh Skeleton shape according to direction

--- src/main/java/net/pixeldreamstudios/valor_blocks/block/SkeletonWallBlock.java
+++ src/main/java/net/pixeldreamstudios/valor_blocks/block/SkeletonWallBlock.java
@@ -12,7 +12,10 @@ import org.jetbrains.annotations.NotNull;
 
 public class SkeletonWallBlock extends ValorBlock {
   public static final MapCodec<SkeletonWallBlock> CODEC = simpleCodec(SkeletonWallBlock::new);
-  public static final VoxelShape SHAPE = Block.box(2, 0, 10, 14, 14, 15);
+  protected static final VoxelShape NORTH_SHAPE = Block.box(2, 0, 10, 14, 14, 15);
+  protected static final VoxelShape SOUTH_SHAPE = Block.box(2, 0, 1, 14, 14, 6);
+  protected static final VoxelShape WEST_SHAPE = Block.box(10, 0, 2, 15, 14, 14);
+  protected static final VoxelShape EAST_SHAPE = Block.box(1, 0, 2, 6, 14, 14);
 
   public SkeletonWallBlock(Properties properties) {
     super(properties);
@@ -26,6 +29,11 @@ public class SkeletonWallBlock extends ValorBlock {
   @Override
   public @NotNull VoxelShape getShape(
       BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
-    return SHAPE;
+    return switch (state.getValue(FACING)) {
+      case SOUTH -> SOUTH_SHAPE;
+      case WEST -> WEST_SHAPE;
+      case EAST -> EAST_SHAPE;
+      default -> NORTH_SHAPE;
+    };
   }
 }
